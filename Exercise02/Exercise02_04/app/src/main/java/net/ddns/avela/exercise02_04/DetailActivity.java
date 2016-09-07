432a49f475812cf165ae6b805cd741a46a90b8e3
Miika Avela <h4211@student.jamk.fi>
2016-09-07 21:08:52 +0300

    Exercise02_04 - ListView example/exercise OK
    Exercise done

---
+++ Exercise02/Exercise02_04/app/src/main/java/net/ddns/avela/exercise02_04/DetailActivity.java
@@ -0,0 +1,45 @@
+package net.ddns.avela.exercise02_04;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by Kotimonni on 7.9.2016.
+ */
+public class DetailActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // draw activity detail layout
+        setContentView(R.layout.activity_detail);
+        // get intent which has used to open this activity
+        Intent intent = getIntent();
+        // get data from intent
+        Bundle bundle = intent.getExtras();
+        // get phone name
+        String phone = bundle.getString("phone");
+        // update text and image views to show data
+        TextView textView = (TextView) findViewById(R.id.text0);
+        textView.setText(phone);
+        ImageView imageView = (ImageView) findViewById(R.id.image0);
+        // show phone image
+        switch (phone) {
+            case "Android": imageView.setImageResource(R.drawable.android); break;
+            case "iPhone": imageView.setImageResource(R.drawable.ios); break;
+            case "WindowsMobile": imageView.setImageResource(R.drawable.windows); break;
+            case "Blackberry": imageView.setImageResource(R.drawable.blackberry); break;
+            case "WebOS": imageView.setImageResource(R.drawable.webos); break;
+            case "Ubuntu": imageView.setImageResource(R.drawable.ubuntu); break;
+        }
+    }
+
+    public void backButtonPressed(View view) {
+        //finish and close activity
+        finish();
+    }
+
+}
